71320704defe6f3570cb0dd7b9ed42c014dfc58f
Fu Carine <carine.fu@etud.u-picardie.fr>
2026-05-22 16:00:06 +0200

    Ajoutez des fichiers projet.

---
+++ SAEIHM/Menu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SAEIHM
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnaide_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageAide());
+        }
+
+        private void btnhisto_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageHisto());
+        }
+
+        private void btnjouer_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PageJouer1());
+        }
+
+        private void btnquitter_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
+
+
---
+++ SAEIHM/PageJouer1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAEIHM
+{
+    /// <summary>
+    /// Logique d'interaction pour PageJouer1.xaml
+    /// </summary>
+    public partial class PageJouer1 : Page
+    {
+        public PageJouer1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnretour_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Menu());
+        }
+    }
+}
