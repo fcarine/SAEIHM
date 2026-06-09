4ea5e67fa56b3a7e5c47aeda0172e5e9d830618c
Fu Carine <carine.fu@etud.u-picardie.fr>
2026-06-09 15:51:16 +0200

    ajout classe

--- Systeme/Grid/GridOption.cs
+++ Systeme/Grid/GridOption.cs
@@ -5,19 +5,41 @@ using System.Text;
 
 namespace Systeme.Grid
 {
-    public class GridOption
+    public class GridOption : INotifyPropertyChanged
     {
-        public string Label { get; set; }
-        public int Rows { get; set; }
-        public int Columns { get; set; }
+        private string label;
+        private int rows;
+        private int cols;
 
-        public GridOption(string label, int rows, int columns)
+        public string Label
+        {
+            get => label;
+            set { label = value; OnPropertyChanged(nameof(Label)); }
+        }
+
+        public int Rows
+        {
+            get => rows;
+            set { rows = value; OnPropertyChanged(nameof(Rows)); }
+        }
+
+        public int Cols
+        {
+            get => cols;
+            set { cols = value; OnPropertyChanged(nameof(Cols)); }
+        }
+
+        public GridOption(string label, int rows, int cols)
         {
             Label = label;
             Rows = rows;
-            Columns = columns;
+            Cols = cols;
         }
 
         public override string ToString() => Label;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
---
+++ Systeme/Jeu/Cellule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Systeme.Joueur;
+
+namespace Systeme.Jeu
+{
+
+    using System.ComponentModel;
+
+    namespace Systeme.Jeu
+    {
+        public class Cellule : INotifyPropertyChanged
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            private int etat;
+            public int Etat
+            {
+                get => etat;
+                set { etat = value; OnPropertyChanged(nameof(Etat)); }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged(string name)
+                => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
